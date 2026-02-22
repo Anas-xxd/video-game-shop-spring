fd9e95813dc90907eff751c58e74430ee788fa1c
nunu <anas.xx200@gmail.com>
2026-02-23 02:43:42 +0300

    Initial Spring Boot project

---
+++ src/test/java/com/anasxxd/videogameshop/VideogameshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasxxd.videogameshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
